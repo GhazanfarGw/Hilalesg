f3081fed29d79ef91e76a4d4de297d380b957270
Dan Van <116085344+GhazanfarGw@users.noreply.github.com>
2024-10-27 14:09:20 -0700

    Add files via upload

---
+++ src/Pages/Home/intro.jsx
@@ -0,0 +1,54 @@
+// import React from 'react'
+// import Countdown from '../../countdown';
+
+// function intro() {
+//   return (
+//     <div className='max-w-screen-xl overflow-hidden items-center justify-center mx-auto'>
+//         <div className='md:pt-80 md:px-20 px-5 pt-10'>
+//             <h1 className='third md:text-6xl text-4xl text-[#1FB689] font-light pt-3'>
+//                 Introduction
+//             </h1>
+//             <div className='md:mt-16 mt-8 max-w-screen-lg border-b border-[#000033] pb-10 md:pb-20'>
+//                 <p className='fourth md:text-xl text-lg text-[#2F3239]'>
+//                     Hilal ESG Holdings LTD is in process of obtaining an advisory license for asset management services. This license will enable the company to provide financial guidance and manage client assets in accordance with the applicable regulatory standards. We will put a countdown of 90 days showing our process that our application is submitted to the regulator.
+//                 </p>
+//                 <div className="pt-3">
+//                     <Countdown/>
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default intro
+
+import React from "react";
+import Countdown from "./countdown";
+
+function Description() {
+  return (
+    <>
+    <div className="max-w-screen-xl md:px-10 px-5 mx-auto">
+      <div className="md:pt-40 pt-32">
+        <div className="first">
+          <h1 className="md:text-4xl font-light text-2xl text-[#444444]">
+            What is the Hilal ESG?
+          </h1>
+        </div>
+
+        <div className="second max-w-screen-lg pt-5 md:pt-8 text-[#777]">
+          <p className="md:text-lg text-sm">
+            Hilal ESG Holdings Ltd is diligently pursuing the necessary regulatory licenses to operate across various jurisdictions. Our unwavering commitment to compliance and regulatory standards reflects our dedication to delivering trusted, high-quality financial services to our clients.
+            <br/><br/>
+            As we finalize our licensing process, we will issue an official press release to share the news. In the meantime, we invite you to stay informed about our progress. Simply enter your email below and click "Subscribe" to receive updates directly to your inbox. Thank you for your continued support.
+          </p>
+        </div>
+      </div>
+      <div className="border-b md:pb-10 pb-5"/>
+    </div>
+    </>
+  );
+}
+
+export default Description;
---
+++ src/Pages/Philosophy/Hilalph.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Description() {
+  return (
+    <>
+    <div className="mx-auto justify-between pt-28 md:pt-52 max-w-screen-xl md:px-10 px-5 pb-32">
+      <div className="">
+        <div className="max-w-screen-lg pt-5 md:pt-8 text-[#777] third">
+          <p className="md:text-lg text-sm first-letter:text-5xl">
+          At Hilal ESG Holdings Ltd, we are redefining asset management with a mission that reaches beyond conventional finance. Our goal is to create a fair and inclusive landscape where deserving enterprises access the capital they need, generating exceptional returns for investors through Sharia-compliant and sustainable practices.
+            <br/>
+            <br/>
+            At the core of our approach is the belief that asset management can act as a catalyst for meaningful development, particularly across the Global South. We channel resources to areas where they can drive the most impact, measuring success not solely in financial gains but in the capacity to empower communities and stimulate economic progress.
+            <br/>
+            <br/>
+            We recognize the substantial demand for Sharia-compliant investment options in regions where access to capital remains limited. This approach is not simply philanthropic; rather, we are guided by the principle that responsible investing delivers both impressive returns and social benefit, creating shared value for investors and society alike.
+            <br />
+            <br />
+            As we prepare to launch our regulated advisory and asset management services, our dedication to transparency, responsibility, and integrity is unwavering. Our team is committed to offering strategic financial guidance and asset management solutions that uphold the highest standards of regulatory compliance.
+            <br />
+            <br />
+            Our investment focus spans sectors essential for future resilience and prosperity, including food security, healthcare, artificial intelligence, technology, and renewable energy. Each investment is aligned with Environmental, Social, and Governance (ESG) principles, reflecting our commitment to ethical practices and Sharia compliance.
+            <br />
+            <br />
+            Our team brings a distinguished track record in facilitating cross-border growth, particularly within the Middle East. Our expertise in private equity and venture capital fuels the success of businesses in these markets, supported by clear and precise communication throughout the investment process.
+            <br />
+            <br />
+            We remain resolutely focused on delivering results. With a unique blend of global insight and an acute understanding of diverse legal and economic landscapes, we are poised to identify the optimal financial and regulatory conditions to meet the needs of your business. Our commitment to providing comprehensive Sharia and ESG compliance guidance ensures that every transaction upholds the highest ethical standards, advancing both sustainability and long-term value.
+            <br />
+            <br />
+            We remain resolutely focused on achieving results. Our unique blend of global insights and in-depth knowledge of diverse legal and economic landscapes positions us to identify optimal financial and regulatory conditions tailored to your business's needs. Moreover, our commitment to providing Sharia compliance and ESG compliance feedback for every transaction ensures that your investments uphold the highest ethical standards while advancing sustainability.
+          </p>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Description;
